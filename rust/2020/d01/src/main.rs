4cc2f74fd2f6fcb37ff622a7e70157505f679939
Steffen R. Knollmann <srk@k7n.dev>
2020-12-01 21:19:18 +0000

    refactor: split searching for numbers and reporting

--- rust/2020/d01/src/main.rs
+++ rust/2020/d01/src/main.rs
@@ -8,16 +8,20 @@ fn read<R: Read>(io: R) -> Result<Vec<i64>, Error> {
         .collect()
 }
 
-
-fn main() -> Result<(), Error> {
-    let vec = read(File::open("input.txt")?)?;
+fn part_1(vec: &Vec<i64>) -> Option<(i64, i64)> {
     for i in 0..(vec.len()-1) {
         for j in (i+1)..vec.len() {
             if vec[i] + vec[j] == 2020 {
-                println!("{} + {} = 2020, {} * {} = {}", vec[i], vec[j], vec[i], vec[j], vec[i] * vec[j]);
-                return Ok(());
+                return Some((vec[i], vec[j]));
             }
         }
     }
+    return None;
+}
+
+fn main() -> Result<(), Error> {
+    let vec = read(File::open("input.txt")?)?;
+    let (a, b) = part_1(&vec).unwrap();
+    println!("{} + {} = 2020, {} * {} = {}", a, b, a, b, a*b);
     Ok(())
 }

91408981c371cf54ec6716167cb1fa433b184689
Steffen R. Knollmann <srk@k7n.dev>
2020-12-01 21:21:49 +0000

    feat: solution for 2020 day 1 part 2

--- rust/2020/d01/src/main.rs
+++ rust/2020/d01/src/main.rs
@@ -19,9 +19,23 @@ fn part_1(vec: &Vec<i64>) -> Option<(i64, i64)> {
     return None;
 }
 
+fn part_2(vec: &Vec<i64>) -> Option<(i64, i64, i64)> {
+    for i in 0..(vec.len()-2) {
+        for j in (i+1)..(vec.len()-1) {
+            for k in (j+2)..vec.len() {
+                if vec[i] + vec[j] + vec[k] == 2020 {
+                    return Some((vec[i], vec[j], vec[k]));
+                }
+            }
+        }
+    }
+    return None;
+}
 fn main() -> Result<(), Error> {
     let vec = read(File::open("input.txt")?)?;
     let (a, b) = part_1(&vec).unwrap();
     println!("{} + {} = 2020, {} * {} = {}", a, b, a, b, a*b);
+    let (a, b, c) = part_2(&vec).unwrap();
+    println!("{} + {} + {} = 2020, {} * {} * {} = {}", a, b, c, a, b, c, a*b*c);
     Ok(())
 }

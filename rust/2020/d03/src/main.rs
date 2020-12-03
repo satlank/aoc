17dcf86c78de871f53b5f6b55e4bdce5298ddf28
Steffen R. Knollmann <srk@k7n.dev>
2020-12-03 08:38:18 +0000

    feat: solution for 2020 day 3 part 2

--- rust/2020/d03/src/main.rs
+++ rust/2020/d03/src/main.rs
@@ -51,9 +51,25 @@ fn part1(field: &Field) -> usize {
     field.collisions(1, 3)
 }
 
+fn part2(field: &Field) -> usize {
+    let mut cols = 1;
+    let options = vec![
+        (1, 1),
+        (1, 3),
+        (1, 5),
+        (1, 7),
+        (2, 1)
+    ];
+    for (row_inc, col_inc) in options.iter() {
+        cols *= field.collisions(*row_inc, *col_inc);
+    }
+    cols
+}
+
 fn main() -> Result<(), Error> {
     let field = Field::parse("input.txt")?;
     println!("Collisions in part 1: {}", part1(&field));
+    println!("Collisions in part 2: {} ", part2(&field));
     Ok(())
 }
 

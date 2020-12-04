b945780aff2d8dcbd080ef43d29ece21a8216018
Steffen R. Knollmann <srk@k7n.dev>
2020-12-04 09:10:52 +0000

    feat: solution for 2020 day 4 part 2

--- rust/2020/d04/src/main.rs
+++ rust/2020/d04/src/main.rs
@@ -38,6 +38,56 @@ impl Passport {
     fn valid(&self) -> bool {
         self.byr.is_some() && self.iyr.is_some() && self.eyr.is_some() && self.hgt.is_some() && self.hcl.is_some() && self.ecl.is_some() && self.pid.is_some()
     }
+
+    fn valid2(&self) -> bool {
+        if !self.valid() {
+            return false;
+        }
+        validate_year(self.byr.as_ref().unwrap(), 1920, 2002)
+            && validate_year(self.iyr.as_ref().unwrap(), 2010, 2020)
+            && validate_year(self.eyr.as_ref().unwrap(), 2020, 2030)
+            && validate_height(self.hgt.as_ref().unwrap())
+            && validate_hcl(self.hcl.as_ref().unwrap())
+            && validate_ecl(self.ecl.as_ref().unwrap())
+            && validate_pid(self.pid.as_ref().unwrap())
+    }
+}
+
+fn validate_year<S: AsRef<str>>(year: S, min: usize, max: usize) -> bool {
+    let val = match scan_fmt!(year.as_ref(), "{}", usize) {
+        Ok(x) => x,
+        Err(_) => return false,
+    };
+    val >= min && val <= max
+}
+
+fn validate_height<S: AsRef<str>>(hgt: S) -> bool {
+    let (val, unit) = match scan_fmt!(hgt.as_ref(), "{d}{}", usize, String) {
+        Ok(x) => x,
+        Err(_) => return false,
+    };
+    (unit == "cm" && val >= 150 && val <= 193) || (unit == "in" && val >= 59 && val <= 76)
+}
+
+fn validate_hcl<S: AsRef<str>>(hcl: S) -> bool {
+    let hex = match scan_fmt!(hcl.as_ref(), "#{/[0-9a-f]+/}", String) {
+        Ok(x) => x,
+        Err(_) => return false,
+    };
+    hex.len() == 6
+}
+
+fn validate_ecl<S: AsRef<str>>(ecl: S) -> bool {
+    let ecl = ecl.as_ref();
+    ecl == "amb" || ecl == "blu" || ecl == "brn" || ecl == "gry" || ecl == "grn" || ecl == "hzl" || ecl == "oth"
+}
+
+fn validate_pid<S: AsRef<str>>(pid: S) -> bool {
+    let digits = match scan_fmt!(pid.as_ref(), "{/[0-9]+/}", String) {
+        Ok(x) => x,
+        Err(_) => return false,
+    };
+    digits.len() == 9
 }
 
 fn read<R: Read>(io: R) -> Result<Vec<Passport>, Error> {
@@ -59,9 +109,14 @@ fn part1(vec: &Vec<Passport>) -> usize {
     vec.iter().filter(|&p| p.valid()).count()
 }
 
+fn part2(vec: &Vec<Passport>) -> usize {
+    vec.iter().filter(|&p| p.valid2()).count()
+}
+
 fn main() -> Result<(), Error> {
     let vec = read(File::open("input.txt")?)?;
     println!("Number of valid passports: {}", part1(&vec));
+    println!("Number of valid passports part 2: {}", part2(&vec));
     Ok(())
 }
 
@@ -97,5 +152,38 @@ mod tests {
             .unwrap()
             .valid()
         );
+    }
+
+    #[test]
+    fn test_valid_year() {
+        assert!(validate_year("2002", 1920, 2002));
+        assert!(!validate_year("2003", 1920, 2002));
+    }
+
+    #[test]
+    fn test_valid_hgt() {
+        assert!(validate_height("60in"));
+        assert!(validate_height("190cm"));
+        assert!(!validate_height("190in"));
+        assert!(!validate_height("190"));
+    }
+
+    #[test]
+    fn test_valid_hcl() {
+        assert!(validate_hcl("#123abc"));
+        assert!(!validate_hcl("#123abz"));
+        assert!(!validate_hcl("123abc"));
+    }
+
+    #[test]
+    fn test_valid_ecl() {
+        assert!(validate_ecl("brn"));
+        assert!(!validate_ecl("wat"));
+    }
+
+    #[test]
+    fn test_valid_pid() {
+        assert!(validate_pid("000000001"));
+        assert!(!validate_pid("0123456789"));
     }
 }

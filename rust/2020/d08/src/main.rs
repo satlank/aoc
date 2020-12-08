0f4a281668e3465a16b71ba9a517738754e65a8c
Steffen R. Knollmann <srk@k7n.dev>
2020-12-08 08:25:23 +0000

    feat: solution for 2020 day 8 part 1

---
+++ rust/2020/d08/src/main.rs
@@ -0,0 +1,72 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
+
+#[macro_use]
+extern crate scan_fmt;
+
+enum OpName {
+    ACC, NOP, JMP
+}
+
+struct Operation {
+    op: OpName,
+    value: i32,
+}
+
+impl Operation {
+    fn parse<S: AsRef<str>>(line: S) -> Result<Operation, scan_fmt::parse::ScanError> {
+        let line = line.as_ref();
+        let (op_name, sign, value) = scan_fmt!(line, "{/acc|jmp|nop/} {/[+-]/}{d}", String, char, i32)?;
+        Ok(Operation {
+            op: match op_name.as_ref() {
+                "acc" => OpName::ACC,
+                "nop" => OpName::NOP,
+                "jmp" => OpName::JMP,
+                _ => panic!("Huh")
+            },
+            value: value * (if sign == '-' { -1 } else {1})
+        })
+    }
+}
+
+fn read<R: Read>(io: R) -> Result<Vec<Operation>, Error> {
+    BufReader::new(io)
+        .lines()
+        .map(|line| {
+            line.and_then(|row| Operation::parse(row).map_err(|e| Error::new(ErrorKind::InvalidData, e)))
+        })
+        .collect()
+}
+
+fn part1(prog: &[Operation]) -> i32 {
+    let mut acc = 0;
+    let mut op_cntr = 0;
+    let mut executed = HashSet::new();
+    loop {
+        if executed.contains(&op_cntr) {
+            break;
+        }
+        executed.insert(op_cntr);
+        match prog[op_cntr].op {
+            OpName::ACC => {
+                acc += prog[op_cntr].value;
+                op_cntr += 1;
+            },
+            OpName::JMP => {
+                op_cntr = (op_cntr as i32 + prog[op_cntr].value) as usize;
+            },
+            OpName::NOP => {
+                op_cntr += 1;
+            },
+        }
+    }
+    acc
+}
+
+fn main() -> Result<(), Error> {
+    let vec = read(File::open("input.txt")?)?;
+    println!("Read {} instructions", vec.len());
+    println!("Accumulation before infinite loop: {}", part1(&vec));
+    Ok(())
+}

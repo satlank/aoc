7edd7474a7767e331b46feed2b24ae3b8725fd0e
Steffen R. Knollmann <srk@k7n.dev>
2020-12-09 06:13:20 +0000

    refactor: use option return type

--- rust/2020/d09/src/main.rs
+++ rust/2020/d09/src/main.rs
@@ -9,45 +9,45 @@ fn read<R: Read>(io: R) -> Result<Vec<usize>, Error> {
 }
 
 fn is_sum(vec: &[usize], val: usize) -> bool {
-    for i in 0..vec.len()-1 {
-        for j in i+1..vec.len() {
+    for i in 0..vec.len() - 1 {
+        for j in i + 1..vec.len() {
             if vec[i] + vec[j] == val {
-                return true
+                return true;
             }
         }
     }
-    return false;
+    false
 }
 
-fn part1(vec: &[usize]) -> usize {
+fn part1(vec: &[usize]) -> Option<usize> {
     for i in 25..vec.len() {
-        if !is_sum(&vec[i-25..i], vec[i]) {
-            return vec[i];
+        if !is_sum(&vec[i - 25..i], vec[i]) {
+            return Some(vec[i]);
         }
     }
-    0
+    None
 }
 
-fn part2(vec: &[usize], val: usize) -> usize {
-    for i in 0..vec.len()-1 {
+fn part2(vec: &[usize], val: usize) -> Option<usize> {
+    for i in 0..vec.len() - 1 {
         let mut sum = vec[i];
-        let mut j = i+1;
+        let mut j = i + 1;
         while sum < val {
             sum += vec[j];
             j += 1;
         }
         if sum == val {
-            return vec[i..=j].iter().min().unwrap() + vec[i..=j].iter().max().unwrap();
+            return Some(vec[i..=j].iter().min().unwrap() + vec[i..=j].iter().max().unwrap());
         }
     }
-    0
+    None
 }
 
 fn main() -> Result<(), Error> {
     let vec = read(File::open("input.txt")?)?;
     println!("Read {} numbers", vec.len());
-    let val = part1(&vec);
+    let val = part1(&vec).unwrap();
     println!("{} is not a sum of two preamble values", val);
-    println!("Encryption weakness is {}", part2(&vec, val));
+    println!("Encryption weakness is {}", part2(&vec, val).unwrap());
     Ok(())
 }

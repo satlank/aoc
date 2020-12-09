a35965a9cd9940e0abc4ce5f85f95a7016eac547
Steffen R. Knollmann <srk@k7n.dev>
2020-12-09 05:54:11 +0000

    feat: solution for 2020 day 9

---
+++ rust/2020/d09/src/main.rs
@@ -0,0 +1,53 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
+
+fn read<R: Read>(io: R) -> Result<Vec<usize>, Error> {
+    let br = BufReader::new(io);
+    br.lines()
+        .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
+        .collect()
+}
+
+fn is_sum(vec: &[usize], val: usize) -> bool {
+    for i in 0..vec.len()-1 {
+        for j in i+1..vec.len() {
+            if vec[i] + vec[j] == val {
+                return true
+            }
+        }
+    }
+    return false;
+}
+
+fn part1(vec: &[usize]) -> usize {
+    for i in 25..vec.len() {
+        if !is_sum(&vec[i-25..i], vec[i]) {
+            return vec[i];
+        }
+    }
+    0
+}
+
+fn part2(vec: &[usize], val: usize) -> usize {
+    for i in 0..vec.len()-1 {
+        let mut sum = vec[i];
+        let mut j = i+1;
+        while sum < val {
+            sum += vec[j];
+            j += 1;
+        }
+        if sum == val {
+            return vec[i..=j].iter().min().unwrap() + vec[i..=j].iter().max().unwrap();
+        }
+    }
+    0
+}
+
+fn main() -> Result<(), Error> {
+    let vec = read(File::open("input.txt")?)?;
+    println!("Read {} numbers", vec.len());
+    let val = part1(&vec);
+    println!("{} is not a sum of two preamble values", val);
+    println!("Encryption weakness is {}", part2(&vec, val));
+    Ok(())
+}

7da75f064c5505ca558fc1601e742ce692d9b7a7
Steffen R. Knollmann <srk@k7n.dev>
2022-12-16 23:08:52 +0000

    feat(rs): solution for 2022 day 16 part 2

--- rust/2022/d16/src/main.rs
+++ rust/2022/d16/src/main.rs
@@ -1,5 +1,10 @@
 // https://adventofcode.com/2022/day/16
+// This is awful, clearly didn't get the trick today.  Couldn't come up with a clever algorithm, so
+// it is brute-forced (though not the totally naïve way of running over all permutations of how to
+// visit the valves).  Plenty of scope to reduce duplication and almost-but-not-quite-the-same
+// code, but don't have any energy left today.
 
+use itertools::Itertools;
 use std::{
     collections::{HashMap, HashSet},
     fs::File,
@@ -105,72 +110,412 @@ fn build_distance_graph(
         .collect()
 }
 
-fn walk(
-    step: usize,
+fn get_options(
     current: &str,
-    current_flow: usize,
-    map: &HashMap<String, Valve>,
+    remaining: &HashSet<String>,
     distance_graph: &HashMap<String, HashMap<String, usize>>,
-    mut turned_on: HashSet<String>,
-) -> usize {
-    //println!(
-    //    "Valve {}, Step {}, current_flow: {}, turned_on: {:?}",
-    //    current, step, current_flow, turned_on
-    //);
-    let step_spent = if current != "AA" {
-        turned_on.insert(current.to_string());
-        1
-    } else {
-        0
-    };
-
-    let local_flow = map[current].flow_rate;
-    let steps_left = 31 - (step + step_spent);
-
-    let options = distance_graph[current]
+    steps_left: usize,
+) -> HashSet<String> {
+    distance_graph[current]
         .keys()
         .filter_map(|v| {
-            if turned_on.contains(v) || distance_graph[current][v] >= steps_left {
-                None
-            } else {
+            if remaining.contains(v) && distance_graph[current][v] + 1 < steps_left {
                 Some(v.to_string())
+            } else {
+                None
             }
         })
-        .collect::<HashSet<_>>();
-    if options.len() == 0 {
-        return step_spent * current_flow + steps_left * (current_flow + local_flow);
-    }
-    if current != "AA" {
-        assert!(turned_on.contains(current));
-    }
+        .collect::<HashSet<_>>()
+}
 
-    options
-        .iter()
-        .map(|go_to_valve| {
-            let distance = distance_graph[current][go_to_valve];
-            current_flow + 
-            distance * (current_flow + local_flow)
-                + walk(
-                    step + 1 + distance,
-                    go_to_valve,
-                    current_flow + local_flow,
-                    map,
-                    distance_graph,
-                    turned_on.clone(),
-                )
-        })
-        .max()
-        .unwrap()
+fn walking_to_1(
+    walker: Option<(&str, usize)>,
+    remaining: HashSet<String>,
+    step: usize,
+    mut current_flow: usize,
+    map: &HashMap<String, Valve>,
+    distance_graph: &HashMap<String, HashMap<String, usize>>,
+) -> usize {
+    let steps_left = 29 - step;
+    match walker {
+        Some(w1) => {
+            let new_w1 = if w1.1 > 0 {
+                Some((w1.0, w1.1 - 1))
+            } else {
+                current_flow += map[w1.0].flow_rate;
+                None
+            };
+            if new_w1.is_none() {
+                let options = get_options(w1.0, &remaining, distance_graph, steps_left);
+                return current_flow
+                    + options
+                        .iter()
+                        .map(|v| {
+                            let d = distance_graph[w1.0][v];
+                            let new_remaining = remaining
+                                .clone()
+                                .into_iter()
+                                .filter_map(|tmp| if &tmp != v { Some(tmp) } else { None })
+                                .collect::<HashSet<_>>();
+
+                            walking_to_1(
+                                Some((v, d)),
+                                new_remaining,
+                                step + 1,
+                                current_flow,
+                                map,
+                                distance_graph,
+                            )
+                        })
+                        .max()
+                        .unwrap_or(steps_left * current_flow);
+            } else {
+                return current_flow
+                    + walking_to_1(
+                        new_w1,
+                        remaining,
+                        step + 1,
+                        current_flow,
+                        map,
+                        distance_graph,
+                    );
+            }
+        }
+        None => {
+            if remaining.is_empty() {
+                return current_flow * steps_left;
+            } else {
+                // Initialisation, we are in AA
+                let options = get_options("AA", &remaining, distance_graph, steps_left);
+                return current_flow
+                    + options
+                        .iter()
+                        .map(|v| {
+                            let d = distance_graph["AA"][v];
+                            let new_remaining = remaining
+                                .clone()
+                                .into_iter()
+                                .filter_map(|tmp| if &tmp != v { Some(tmp) } else { None })
+                                .collect::<HashSet<_>>();
+                            walking_to_1(
+                                Some((v, d)),
+                                new_remaining,
+                                step + 1,
+                                current_flow,
+                                map,
+                                distance_graph,
+                            )
+                        })
+                        .max()
+                        .unwrap();
+            }
+        }
+    }
+}
+
+fn combine_options(
+    opt1: HashSet<String>,
+    opt2: HashSet<String>,
+) -> Vec<(Option<String>, Option<String>)> {
+    if opt1.is_empty() && opt1.is_empty() {
+        return Vec::new();
+    }
+    if opt1.is_empty() && !opt2.is_empty() {
+        return opt2.into_iter().map(|o2| (None, Some(o2))).collect();
+    }
+    if !opt1.is_empty() && opt2.is_empty() {
+        return opt1.into_iter().map(|o1| (Some(o1), None)).collect();
+    }
+    opt1.iter()
+        .flat_map(|o1| opt2.iter().map(|o2| (Some(o1.clone()), Some(o2.clone()))))
+        .filter_map(|(x, y)| if x != y { Some((x, y)) } else { None })
+        .collect()
+}
+
+fn walking_to(
+    walker1: Option<(&str, usize)>,
+    walker2: Option<(&str, usize)>,
+    remaining: HashSet<String>,
+    step: usize,
+    mut current_flow: usize,
+    map: &HashMap<String, Valve>,
+    distance_graph: &HashMap<String, HashMap<String, usize>>,
+) -> usize {
+    let steps_left = 25 - step;
+    match (walker1, walker2) {
+        (Some(w1), Some(w2)) => {
+            let new_w1 = if w1.1 > 0 {
+                Some((w1.0, w1.1 - 1))
+            } else {
+                current_flow += map[w1.0].flow_rate;
+                None
+            };
+            let new_w2 = if w2.1 > 0 {
+                Some((w2.0, w2.1 - 1))
+            } else {
+                current_flow += map[w2.0].flow_rate;
+                None
+            };
+            if new_w1.is_none() && new_w2.is_none() {
+                let options1 = get_options(w1.0, &remaining, distance_graph, steps_left);
+                let options2 = get_options(w2.0, &remaining, distance_graph, steps_left);
+                let comb_opts = combine_options(options1, options2);
+
+                let recursion = comb_opts
+                    .into_iter()
+                    .map(|(mo1, mo2)| {
+                        let w1_tmp = if let Some(o1) = &mo1 {
+                            Some((o1.as_ref(), distance_graph[w1.0][o1]))
+                        } else {
+                            None
+                        };
+                        let w2_tmp = if let Some(o2) = &mo2 {
+                            Some((o2.as_ref(), distance_graph[w2.0][o2]))
+                        } else {
+                            None
+                        };
+                        let new_remaining = remaining
+                            .clone()
+                            .into_iter()
+                            .filter_map(|tmp| {
+                                if tmp != mo1.clone().unwrap_or("".to_string())
+                                    && tmp != mo2.clone().unwrap_or("".to_string())
+                                {
+                                    Some(tmp)
+                                } else {
+                                    None
+                                }
+                            })
+                            .collect::<HashSet<_>>();
+                        walking_to(
+                            w1_tmp,
+                            w2_tmp,
+                            new_remaining,
+                            step + 1,
+                            current_flow,
+                            map,
+                            distance_graph,
+                        )
+                    })
+                    .max()
+                    .unwrap_or(steps_left * current_flow);
+                return recursion + current_flow;
+            } else if new_w1.is_none() && new_w2.is_some() {
+                let options1 = get_options(w1.0, &remaining, distance_graph, steps_left);
+                if options1.is_empty() {
+                    return current_flow
+                        + walking_to(
+                            None,
+                            new_w2,
+                            remaining,
+                            step + 1,
+                            current_flow,
+                            map,
+                            distance_graph,
+                        );
+                } else {
+                    let recursion = options1
+                        .iter()
+                        .map(|o1| {
+                            let d = distance_graph[w1.0][o1];
+                            let new_remaining = remaining
+                                .clone()
+                                .into_iter()
+                                .filter_map(|tmp| if &tmp != o1 { Some(tmp) } else { None })
+                                .collect::<HashSet<_>>();
+                            walking_to(
+                                Some((o1, d)),
+                                new_w2,
+                                new_remaining,
+                                step + 1,
+                                current_flow,
+                                map,
+                                distance_graph,
+                            )
+                        })
+                        .max()
+                        .unwrap();
+                    return current_flow + recursion;
+                }
+            } else if new_w1.is_some() && new_w2.is_none() {
+                let options2 = get_options(w2.0, &remaining, distance_graph, steps_left);
+                if options2.is_empty() {
+                    return current_flow
+                        + walking_to(
+                            new_w1,
+                            None,
+                            remaining,
+                            step + 1,
+                            current_flow,
+                            map,
+                            distance_graph,
+                        );
+                } else {
+                    let recursion = options2
+                        .iter()
+                        .map(|o2| {
+                            let d = distance_graph[w2.0][o2];
+                            let new_remaining = remaining
+                                .clone()
+                                .into_iter()
+                                .filter_map(|tmp| if &tmp != o2 { Some(tmp) } else { None })
+                                .collect::<HashSet<_>>();
+                            walking_to(
+                                new_w1,
+                                Some((o2, d)),
+                                new_remaining,
+                                step + 1,
+                                current_flow,
+                                map,
+                                distance_graph,
+                            )
+                        })
+                        .max()
+                        .unwrap();
+                    return current_flow + recursion;
+                }
+            } else {
+                return current_flow
+                    + walking_to(
+                        new_w1,
+                        new_w2,
+                        remaining,
+                        step + 1,
+                        current_flow,
+                        map,
+                        distance_graph,
+                    );
+            }
+        }
+        (Some(w1), None) => {
+            let new_w1 = if w1.1 > 0 {
+                Some((w1.0, w1.1 - 1))
+            } else {
+                current_flow += map[w1.0].flow_rate;
+                None
+            };
+            if new_w1.is_none() {
+                let options = get_options(w1.0, &remaining, distance_graph, steps_left);
+                let recursion = options
+                    .iter()
+                    .map(|v| {
+                        let d = distance_graph[w1.0][v];
+                        let new_remaining = remaining
+                            .clone()
+                            .into_iter()
+                            .filter_map(|tmp| if &tmp != v { Some(tmp) } else { None })
+                            .collect::<HashSet<_>>();
+
+                        walking_to(
+                            Some((v, d)),
+                            None,
+                            new_remaining,
+                            step + 1,
+                            current_flow,
+                            map,
+                            distance_graph,
+                        )
+                    })
+                    .max()
+                    .unwrap_or(steps_left * current_flow);
+                return current_flow + recursion;
+            } else {
+                return current_flow
+                    + walking_to(
+                        new_w1,
+                        None,
+                        remaining,
+                        step + 1,
+                        current_flow,
+                        map,
+                        distance_graph,
+                    );
+            }
+        }
+        (None, Some(_)) => {
+            // Flip around so that we end up in Some, None case
+            walking_to(
+                walker2,
+                None,
+                remaining,
+                step,
+                current_flow,
+                map,
+                distance_graph,
+            )
+        }
+        (None, None) => {
+            if remaining.is_empty() {
+                return current_flow * steps_left;
+            } else {
+                // Initialisation, we are in AA
+                let options = get_options("AA", &remaining, distance_graph, steps_left);
+                let recursion = options
+                    .iter()
+                    .permutations(2)
+                    .map(|p| {
+                        let d0 = distance_graph["AA"][p[0]];
+                        let d1 = distance_graph["AA"][p[1]];
+                        let new_remaining = remaining
+                            .clone()
+                            .into_iter()
+                            .filter_map(|tmp| {
+                                if &tmp != p[0] && &tmp != p[1] {
+                                    Some(tmp)
+                                } else {
+                                    None
+                                }
+                            })
+                            .collect::<HashSet<_>>();
+                        let rs = walking_to(
+                            Some((p[0], d0)),
+                            Some((p[1], d1)),
+                            new_remaining,
+                            step + 1,
+                            current_flow,
+                            map,
+                            distance_graph,
+                        );
+                        rs
+                    })
+                    .max()
+                    .unwrap();
+                return current_flow + recursion;
+            }
+        }
+    }
 }
 
 fn part_1(input: &HashMap<String, Valve>) -> usize {
     let distance_graph = build_distance_graph(input, "AA");
+    let remaining = input
+        .iter()
+        .filter_map(|(name, v)| {
+            if v.flow_rate > 0 {
+                Some(name.to_string())
+            } else {
+                None
+            }
+        })
+        .collect::<HashSet<_>>();
 
-    walk(0, "AA", 0, input, &distance_graph, HashSet::new())
+    walking_to_1(None, remaining, 0, 0, input, &distance_graph)
 }
 
 fn part_2(input: &HashMap<String, Valve>) -> usize {
-    input.len()
+    let distance_graph = build_distance_graph(input, "AA");
+    let remaining = input
+        .iter()
+        .filter_map(|(name, v)| {
+            if v.flow_rate > 0 {
+                Some(name.to_string())
+            } else {
+                None
+            }
+        })
+        .collect::<HashSet<_>>();
+
+    walking_to(None, None, remaining, 0, 0, input, &distance_graph)
 }
 
 fn main() {
@@ -188,9 +533,8 @@ mod tests {
     #[test]
     fn example_1() {
         let input = read(File::open("example1.txt").unwrap());
-        println!("{:?}", input);
         assert_eq!(part_1(&input), 1651);
-        //assert_eq!(part_2(&input), 1);
+        assert_eq!(part_2(&input), 1707);
     }
 
     #[test]
@@ -214,7 +558,6 @@ mod tests {
     fn test_build_distance_graph() {
         let input = read(File::open("example1.txt").unwrap());
         let graph = build_distance_graph(&input, "AA");
-        println!("{:?}", graph);
         assert_eq!(graph.len(), 7);
         assert_eq!(graph["AA"].len(), 6);
         assert_eq!(graph["BB"].len(), 5);
